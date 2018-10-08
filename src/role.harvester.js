24dc3b86ca8b2364b57dea908e72d826af0ae002
Voronchikhin Vladislav <voron-chihin@ya.ru>
2018-10-09 02:42:50 +0300

    Create role.harvester.js

---
+++ src/role.harvester.js
@@ -0,0 +1,69 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        
+        if(creep.memory.suply && creep.carry.energy == 0) {
+            creep.memory.suply = false;
+            creep.say('⛏️ harvest');
+	    }
+	    
+	    if(!creep.memory.suply && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.suply = true;
+	        creep.say('🔄 suply');
+	    }
+	    
+	    if(creep.memory.suply) {
+	        
+	        var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.energy < structure.energyCapacity;
+                    }
+            });
+            
+            var containers = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] < structure.storeCapacity);
+            }
+            });
+                    
+            var towers = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity);
+            }
+            });
+            
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else if(towers.length > 0) {
+                 if(creep.transfer(towers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(towers[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else if(containers.length > 0) {
+                 if(creep.transfer(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else {
+                var flags = creep.room.find(FIND_FLAGS, {
+                    filter: (flag) => {
+                        return flag.name == 'harvesters';
+                    }
+                });
+                if(flags.length > 0) {
+    	            creep.moveTo(flags[0]);
+                }
+            }
+	    }
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+	    }
+	}
+};
+
+module.exports = roleHarvester;
